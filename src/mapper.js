cacf7a0023412b26076f7dba98ac442257eec5d8
Miguel Ayala <miguelayala01012014@gmail.com>
2015-10-24 19:42:29 +0200

    added tilelayer

--- src/mapper.js
+++ src/mapper.js
@@ -4,6 +4,8 @@ export default class Mapper extends Leaflet.Class {
   init() {
     const map = Leaflet.map('map');
     map.setView([51.505, -0.09], 13);
-    window.console.log(map);
+    let osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    let osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+    Leaflet.tileLayer(osmUrl, {minZoom: 8, maxZoom: 18, attribution: osmAttrib}).addTo(map);
   }
 }
